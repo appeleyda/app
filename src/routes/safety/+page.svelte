f3585075d2b888a6d249de6102db272a28b169dd
appeleyda <frontdeskat121@two0aks.com>
2023-10-04 22:43:25 +0000

    update

--- src/routes/safety/+page.svelte
+++ src/routes/safety/+page.svelte
@@ -12,9 +12,14 @@
 		<li>Double check balances</li>
 		<li>Make sure you've verified the information</li>
 		<li>Check accounts' feedback score</li>
+		<li>Try to use verified instances when possible</li>
 	</ul>
 
 	<h3>Most importantly, when trading coins, be extremely careful to use the exact address and amount provided. Since TradeUp is decentralized, it's possible for your coins to be lost forever if you don't follow this rule.</h3>
+</div>
+
+<div class="verified">
+	<h1>This is a <span style="color: lightgreen;">verified instance</span>, with <span style="color: var(--color-theme-1);">729</span> successful trades and a <span style="color: var(--color-theme-1);">0.1</span>% dispute rate since its creation in <span style="color: var(--color-theme-1);">January, 2020</span>.</h1>
 </div>
 
 <style>
